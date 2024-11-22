bff188beab5ede545ec88775bd26ecf223742b87
Ethan Chen <echen@lcs.on.ca>
2024-11-22 09:09:36 -0500

    set up remote and created groups and views

---
+++ FavouriteThings/Views/FavouriteThingsView.swift
@@ -0,0 +1,18 @@
+//
+//  FavouriteThingsView.swift
+//  FavouriteThings
+//
+//  Created by Pak Ching Ethan Chen on 2024-11-22.
+//
+
+import SwiftUI
+
+struct FavouriteThingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavouriteThingsView()
+}
