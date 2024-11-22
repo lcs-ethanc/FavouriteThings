12165f4708bfade0a54449f29216567a8f039b35
Ethan Chen <echen@lcs.on.ca>
2024-11-22 09:07:27 -0500

    Initial Commit

---
+++ FavouriteThings/FavouriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteThingsApp.swift
+//  FavouriteThings
+//
+//  Created by Pak Ching Ethan Chen on 2024-11-22.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
