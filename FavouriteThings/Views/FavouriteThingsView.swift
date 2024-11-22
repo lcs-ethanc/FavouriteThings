d1c909a8733748bd5423cc8bbd1e64dbbef423ea
Ethan Chen <echen@lcs.on.ca>
2024-11-22 09:40:23 -0500

    changed font weight and size and added title

--- FavouriteThings/Views/FavouriteThingsView.swift
+++ FavouriteThings/Views/FavouriteThingsView.swift
@@ -10,10 +10,18 @@ import SwiftUI
 struct FavouriteThingsView: View {
     let providedFavThing: FavouriteThing
     var body: some View {
-        Text(favThingOne.thingName)
-        Text(favThingOne.thingDescription)
-        Text(favThingOne.thingImage)
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack(alignment: .leading){
+                Text(favThingOne.thingName)
+                    .fontWeight(.bold)
+                    .font(.system(size: 40))
+                Text(favThingOne.thingDescription)
+                Text(favThingOne.thingImage)
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            } .navigationTitle("My Favourite Things")
+        }
+
+  
     }
 }
 
